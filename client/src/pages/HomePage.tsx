29cb1222cf2c392bd60c3a2417f520594b5b24a3
Saïf Abdelkefi <saifabdelkefi@Saifs-MacBook-Air.local>
2025-07-26 10:18:05 -0400

    fixed scroll in homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,6 +2,50 @@ export default function Home() {
   return (
     <>
       <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is Graphcards</h1>
+      <h1>Hello this is sasbasb</h1>
+      <h1>Hello this is Graphcards</h1>
     </>
   );
 }
